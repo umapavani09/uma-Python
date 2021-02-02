43aa531ff52dd25eba7d6ecd51564f938d215837
umapavani09 <bheemavarapuumapavani@gmail.com>
2021-02-02 15:25:05 +0530

    third commit

---
+++ string.py
@@ -0,0 +1,12 @@
+# Strings
+" " "
+---->Collection of character is called string or
+---->Group of characters is called a String
+---->in python string representation is '' or "" or " " "   " " "
+---->in python string is imutable
+---->in python string is indexed value based
+---->string supports slicing operator------------>':'
+" " "
+
+a=''
+print(type(a))
